fe3069c4484fe96e8430a872cf190f9be536ad6a
Micah Nelthropp <micahnelthropp@gmail.com>
2023-10-16 12:19:43 -0500

    welcome.html

--- js/date-object.js
+++ js/date-object.js
@@ -10,31 +10,49 @@
 // console.log (postDate)
 
 
-// todo:
-// Create an array of months for printing dates
-let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "December"
-];
+// // todo:
+// // Create an array of months for printing dates
+// let months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "December"
+// ];
+//
+// // todo:
+// // Create the date corresponding to your birthday using the JavaScript Date object.
+// //let jsBirthday
+//  let birthDate = new Date(1996,9,6);
+// console.log(birthDate);
+// // todo:
+// // Log your birthday in the format: January 1, 2014 using the JavaScript Date object.
+// // See link below for methods needed:
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#Getter
+// let newBirthDay = new Date(, 6, 1996 );
+// console.log(newBirthDay)
+// console.log('Here is my birthday using JavaScript: ');
 
-// todo:
-// Create the date corresponding to your birthday using the JavaScript Date object.
-//let jsBirthday
- let birthDate = new Date(1996,9,6);
-console.log(birthDate);
-// todo:
-// Log your birthday in the format: January 1, 2014 using the JavaScript Date object.
-// See link below for methods needed:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date#Getter
-let newBirthDay = new Date(, 6, 1996 );
-console.log(newBirthDay)
-console.log('Here is my birthday using JavaScript: ');
+// 1. Function Declaration: Create a function named add that takes two numbers as parameters and returns their sum.
+
+//  const name = (firstName , lastName) => {
+//   let result = `Hi ${firstName}  ${lastName}`;
+//  if(firstName === undefined && lastName === undefined) {
+//    result = "guest"
+//
+//  } else{
+//    return result;
+//   }
+//   return console.log (result);
+// }
+// name();
+
+
+
+
---
+++ js/guessNumber.js
@@ -0,0 +1,22 @@
+// const targetNumber = Math.floor(Math.random()* 100 + 1);
+//
+// let attempts =0;
+// function checkGuess (){
+//     const userGuess = parseInt(prompt("Guess a number between 1 and 100"));
+// if(isNaN(userGuess)|| userGuess < 1 || userGuess > 100){
+//     alert("Please enter a valid number between 1 and 100");
+//      checkGuess()
+// }else {
+//     attempts++;
+// } if(userGuess===targetNumber){
+//     alert(`You got it the number ${targetNumber} in ${attempts} attempts`)
+//         return;
+//     }else {
+//      const hint = userGuess < targetNumber ? "Try a higher number" : "Try a lower number";
+//      alert(`wrong guess ${hint}`);
+//      checkGuess();
+//     }
+//
+//
+//
+// }
