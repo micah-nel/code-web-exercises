81ad06c186e73a6c545e3907728bd43a63451697
Micah Nelthropp <micahnelthropp@gmail.com>
2023-11-07 13:35:28 -0600

    attributes

---
+++ js/attributes-css-js.js
@@ -0,0 +1,20 @@
+const profilePic = document.querySelector("img")
+setTimeout(()=>{
+    profilePic.setAttribute("src", "./img/backGround1.jpeg")
+}, 2000)
+
+const profileName = document.querySelector("h2")
+setTimeout(()=>{
+    profileName.innerHTML = "Micah Nelthropp"
+},4000)
+
+const profileDesc = document.querySelector("#profile-desc")
+setTimeout(()=>{
+    profileDesc.style.color = "blue";
+},6000)
+
+const profileCard = document.querySelector("#profile-card")
+setInterval(()=>{
+    profileCard.classList.toggle("background-color")
+},2000)
+
